c51170da2d1afa4fac1b4a95e69246be7b746907
YouHojoon <dbghwns66@daum.net>
2020-06-07 01:25:35 +0900

    init

---
+++ configServer/src/main/java/ac/kr/smu/tacos/configServer/configServerApplication.java
@@ -0,0 +1,11 @@
+package ac.kr.smu.tacos.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class configServerApplication {
+    public static void main(String args[]){
+        SpringApplication.run(configServerApplication.class, args);
+    }
+}
